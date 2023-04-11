4154c72193b9b3a7175d1245d1d18386cccfeef1
sanki88 <49454380+sanki88@users.noreply.github.com>
2023-04-11 19:37:33 +0200

    create files and keyboard

---
+++ script.js
@@ -0,0 +1,37 @@
+const words = ["jajko", "krowa", "auto", "samochód", "krowa", "piłka", "laptop", "dzik"];
+const keyboardCharacters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+const word = document.querySelector(".word");
+let wordsMaxNumber = Math.floor(Math.random()*words.length);
+const keyboard = document.querySelector(".keyboard");
+
+let currentWord = words[wordsMaxNumber].split("");
+
+    
+
+for(let i=0; i<currentWord.length; i++){
+    console.log(currentWord.length);
+    word.innerHTML+='<div class="theWord">'+currentWord[i]+'</div>'
+}
+
+
+
+keyboardCharacters.forEach(function(letter){
+    keyboard.innerHTML += `
+    <button class="letter">${letter}</button>
+    `;
+
+    if (letter == "p"){
+        keyboard.innerHTML += `<div class="halfLetter"></div>`;
+    }
+    else if(letter == "l"){
+        keyboard.innerHTML += `<div class="oneHalfLetter"></div>`;
+    }
+
+})
+
+addEventListener("click", function(e){
+    const keyLetter = e.target;
+    console.log(keyLetter.textContent);
+    
+})
