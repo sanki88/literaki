bb84993c5d794310a057c5008272327c5f5cdb74
sanki88 <49454380+sanki88@users.noreply.github.com>
2023-04-14 14:23:08 +0200

    Add files via upload
    update app

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const words = ["jajko", "informatyka", "krowa", "auto", "samochód", "krowa", "piłka", "laptop", "dzik"];
+//words for guess
+const words = ["jajko", "informatyka", "ananas", "krowa", "auto", "drzewo", "krowa", "bramka", "laptop", "dzik", "kubek", "portfel", "krokodyl", "guzik", "dywan", "okno", "farba", "ciasto", "spodnie", "trawa", "telefon", "butelka"];
+//keyboard's keys
 const keyboardCharacters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
+//getting divs
 const word = document.querySelector(".word");
-let wordsMaxNumber = Math.floor(Math.random()*words.length);
 const keyboard = document.querySelector(".keyboard");
-
+//random number od key in array
+let wordsMaxNumber = Math.floor(Math.random()*words.length);
+//split the word
 let currentWord = words[wordsMaxNumber].split("");
+//console.log(currentWord);
 
     
-
+//shuffle word and display
 for(let i=0; i<currentWord.length; i++){
     console.log(currentWord.length);
     word.innerHTML+='<div class="theWord"><div class="theKey key_'+currentWord[i]+'">'+currentWord[i]+'</div></div>';
-
 }
 
 
-
+//loop creating keyboard
 keyboardCharacters.forEach(function(letter){
     keyboard.innerHTML += `
     <button class="letter">${letter}</button>
@@ -31,10 +35,29 @@ keyboardCharacters.forEach(function(letter){
 
 })
 
+//if you click keyboard's button
 addEventListener("click", function(e){
     const keyLetter = e.target;
     console.log(keyLetter.textContent);
-
-
+    console.log(currentWord);
     
-})
+    //uncover selected letters
+    currentWord.forEach(function(clickedletter){
+        if(clickedletter==keyLetter.textContent){
+        let uncoverLetter = document.getElementsByClassName("key_"+keyLetter.textContent);
+        let winCheck = document.getElementsByClassName("theKey");
+        
+        if(winCheck.length >= 1){
+            for(let i =0; i<uncoverLetter.length; i++){
+                uncoverLetter[i].classList.remove("theKey");
+                console.log(winCheck.length);
+            }
+            }
+            else{
+                console.log("Wygrałeś");
+            }
+        };
+
+
+        });
+    })
